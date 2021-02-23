27a79751f092234f60d6d231dba5b1ef3756e21f
Chris Kubick <socalledsound@gmail.com>
2021-02-23 12:17:40 -0800

    after components excercise 1

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return (  
+    <Main />
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#0000ff88',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     color: '#ffffff',
+//   }
+// });
---
+++ README.md
@@ -0,0 +1,5 @@
+# nucamp mar2021 cohort practice files
+
+making seperate branches for you all for each step of the way, starting with init
+
+/Users/df/Library/Android/sdk
